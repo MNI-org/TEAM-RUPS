7411244c5622a61b704f160b93e2a024e14d09f8
Najks <nik.damis@gmail.com>
2025-12-07 19:29:08 +0100

    update vite.config.js to allow accessing by domain

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 export default {
     server: {
-        // listen on all IPs so both IP and domain work
         host: '0.0.0.0',
-        port: 8080, // or 5173 if you prefer
+        port: 8080,
+        allowedHosts: [
+            'datamis.eu',
+        ],
     },
     preview: {
         host: '0.0.0.0',
